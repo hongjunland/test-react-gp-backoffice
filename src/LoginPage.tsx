38f85abbe3f9ee647ee20dbcb053b89b6728c353
hongjunland <zunza96@gmail.com>
2023-08-09 01:51:10 +0900

    chore: 부분구현

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,29 +1,59 @@
-import {
-  CredentialResponse,
-  GoogleLogin,
-  GoogleOAuthProvider,
-} from "@react-oauth/google";
+// import {
+//   CredentialResponse,
+//   GoogleLogin,
+//   GoogleOAuthProvider,
+// } from "@react-oauth/google";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+import GoogleLogin, { GoogleLoginProps, GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
 
 export default function LoginPage() {
   const clientId = process.env.REACT_APP_CLIENT_ID || "";
-  const testData = async() => {
-    const response = await axios.post("http://localhost:8080/api/v1/auth/login/google",{accessToken:"zxczxcxzczx"});
-    // const response = await axios.post("http://localhost:8080/api/v1/auth/login",{email:"str@ing", password:"string"});
-    // const response = await axios.post("http://localhost:8080/api/v1/users",{
-    //     "email": "str@ing",
-    //     "name": "string",
-    //     "nickname": "string",
-    //     "password": "string"
-    //   });
+  // const testData = async(res: CredentialResponse) => {
+  //   console.log(res);
+  //   // const response = await axios.post("http://localhost:8080/login/oauth2/code/google",res);
+  //   const response = await axios.get("http://localhost:8080/api/v1/auth/user/profile", res);
+  //   // const response = await axios.post("http://localhost:8080/api/v1/auth/login",{email:"str@ing", password:"string"});
+  //   // const response = await axios.post("http://localhost:8080/api/v1/users",{
+  //   //     "email": "str@ing",
+  //   //     "name": "string",
+  //   //     "nickname": "string",
+  //   //     "password": "string"
+  //   //   });
+  //   console.log(response);
+  //   return response;
+  // };
+  const handleSuccess = async(response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
     console.log(response);
-    return response;
+    const authCode = response.code;
+    try {
+      // 이 코드를 백엔드로 전송
+      const serverResponse = await axios.post('http://localhost:8080/api/auth/google', {
+        code: authCode
+      });
+
+      // 서버에서 반환된 JWT나 다른 응답을 처리
+      console.log(serverResponse.data);
+
+      // 예를 들어 JWT 토큰을 로컬 저장소에 저장할 수 있습니다.
+      localStorage.setItem('token', serverResponse.data.token);
+
+    } catch (error) {
+      console.error('Error sending auth code to server:', error);
+    }
   };
 
+  const handleError = (error: GoogleLoginProps) => {
+    console.error('error');
+  };
   return (
     <div>
-      {/* <button onClick={testData}>테스트 시작</button> */}
+       <GoogleLogin
+        clientId={clientId}
+        onSuccess={handleSuccess}
+        onFailure={handleError}
+        responseType="code"
+      />
+      {/* <button onClick={testData}>테스트 시작</button>
       <GoogleOAuthProvider clientId={clientId}>
         <GoogleLogin
           onSuccess={async(res: CredentialResponse) => {
@@ -32,7 +62,7 @@ export default function LoginPage() {
             //     url: "http://localhost:8080/api/v1/auth/login/google",
             //     data: { accessToken: res.credential },
             // })
-            const response = await testData();
+            const response = await testData(res);
             console.log(response);
             console.log(res);
           }}
@@ -40,7 +70,7 @@ export default function LoginPage() {
             console.log('error');
           }}
         />
-      </GoogleOAuthProvider>
+      </GoogleOAuthProvider> */}
     </div>
   );
 }
