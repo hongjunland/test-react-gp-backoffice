5df2bc0a90b752e475a9a95b34a21df2c1fea350
Hongjun Lee <zunza96@gmail.com>
2023-08-09 17:30:36 +0900

    chore: redirect page

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -5,72 +5,24 @@
 // } from "@react-oauth/google";
 import axios from "axios";
 import GoogleLogin, { GoogleLoginProps, GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-  const clientId = process.env.REACT_APP_CLIENT_ID || "";
-  // const testData = async(res: CredentialResponse) => {
-  //   console.log(res);
-  //   // const response = await axios.post("http://localhost:8080/login/oauth2/code/google",res);
-  //   const response = await axios.get("http://localhost:8080/api/v1/auth/user/profile", res);
-  //   // const response = await axios.post("http://localhost:8080/api/v1/auth/login",{email:"str@ing", password:"string"});
-  //   // const response = await axios.post("http://localhost:8080/api/v1/users",{
-  //   //     "email": "str@ing",
-  //   //     "name": "string",
-  //   //     "nickname": "string",
-  //   //     "password": "string"
-  //   //   });
-  //   console.log(response);
-  //   return response;
-  // };
-  const handleSuccess = async(response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-    console.log(response);
-    const authCode = response.code;
-    try {
-      // 이 코드를 백엔드로 전송
-      const serverResponse = await axios.post('http://localhost:8080/api/auth/google', {
-        code: authCode
-      });
-
-      // 서버에서 반환된 JWT나 다른 응답을 처리
-      console.log(serverResponse.data);
-
-      // 예를 들어 JWT 토큰을 로컬 저장소에 저장할 수 있습니다.
-      localStorage.setItem('token', serverResponse.data.token);
-
-    } catch (error) {
-      console.error('Error sending auth code to server:', error);
-    }
-  };
-
-  const handleError = (error: GoogleLoginProps) => {
-    console.error('error');
-  };
+  const API_BASE_URL = 'http://localhost:8080';
+  const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+  const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
+  
+  const handleLoginWithGoogle = async(e: React.MouseEvent<HTMLAnchorElement>)=>{
+    e.preventDefault();
+    const response = await fetch(GOOGLE_AUTH_URL);
+    const data = await response.json();
+    console.log(data);
+  }
+  
   return (
     <div>
-       <GoogleLogin
-        clientId={clientId}
-        onSuccess={handleSuccess}
-        onFailure={handleError}
-        responseType="code"
-      />
-      {/* <button onClick={testData}>테스트 시작</button>
-      <GoogleOAuthProvider clientId={clientId}>
-        <GoogleLogin
-          onSuccess={async(res: CredentialResponse) => {
-            // const response = await axios({
-            //     method: "post",
-            //     url: "http://localhost:8080/api/v1/auth/login/google",
-            //     data: { accessToken: res.credential },
-            // })
-            const response = await testData(res);
-            console.log(response);
-            console.log(res);
-          }}
-          onError={() => {
-            console.log('error');
-          }}
-        />
-      </GoogleOAuthProvider> */}
+      <Link to={GOOGLE_AUTH_URL}>구글 로그인</Link>
+      {/* <Link to={GOOGLE_AUTH_URL} onClick={handleLoginWithGoogle}>구글 로그인</Link> */}
     </div>
   );
 }
--- src/RedirectPage.tsx
+++ src/RedirectPage.tsx
@@ -1,5 +1,40 @@
-export default function RedirectPage(){
-    return (<div>
-        <h1>Redirect Google!!!!!!!!!!!!!!!!!!!!!!!</h1>
-    </div>)
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function CallbackComponent() {
+    const history = useHistory();
+
+    useEffect(() => {
+        // URL에서 인증 정보 추출
+        const urlParams = new URLSearchParams(window.location.search);
+        const accessToken = urlParams.get('access_token');
+        const tokenType = urlParams.get('token_type');
+        const expiresIn = urlParams.get('expires_in');
+        const state = urlParams.get('state');
+
+        // TODO: `state` 검증
+
+        if (accessToken) {
+            // 토큰 저장 (예: 로컬 스토리지)
+            localStorage.setItem('accessToken', accessToken);
+
+            // 메인 페이지로 리다이렉트
+            history.push('/');
+        } else {
+            // 오류 처리
+            const error = urlParams.get('error');
+            const errorDescription = urlParams.get('error_description');
+            console.error("OAuth2 error:", error, errorDescription);
+            // 오류 페이지로 리다이렉트 또는 사용자에게 알림
+            history.push('/error');
+        }
+    }, [history]);
+
+    return (
+        <div>
+            Processing authentication...
+        </div>
+    );
 }
+
+export default CallbackComponent;
